64419d4ba714598b811aff27975760a5bce89ef9
pavelkv96 <pavelkv96@gmail.com>
2017-10-25 22:47:35 +0300

    Formatted code

--- app/src/main/java/com/github/pavelkv96/hw_16102017/MainActivity.java
+++ app/src/main/java/com/github/pavelkv96/hw_16102017/MainActivity.java
@@ -17,46 +17,42 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 final VersionChecker checker = new VersionChecker();
                 if (!checker.isCorrectVersion()) {
-                    if (checker.isForceUpdate()) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
-                                alertDialogBuilder
-                                        .setTitle("Necessary to update")
-                                        .setMessage("You should update your application here: " + BuildConfig.UPDATE_URL)
-                                        .setCancelable(false)
-                                        .create()
-                                        .show();
-                            }
-                        });
-                    } else {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
-                                alertDialogBuilder
-                                        .setTitle("You want to update?")
-                                        .setMessage("Do you want to update application? ")
-                                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                                            public void onClick(final DialogInterface dialog, final int id) {
-                                                MainActivity.this.finish();
-                                            }
-                                        })
-                                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                                            public void onClick(final DialogInterface dialog, final int id) {
-                                                dialog.cancel();
-                                            }
-                                        })
-                                        .setCancelable(false)
-                                        .create()
-                                        .show();
-                            }
-                        });
-                    }
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                            VersionDialog(alertDialogBuilder, checker);
+                        }
+                    });
                 }
 
             }
         }).start();
+    }
+
+    void VersionDialog(AlertDialog.Builder alertDialogBuilder, VersionChecker checker) {
+        if (checker.isForceUpdate()) {
+            alertDialogBuilder
+                    .setTitle("Necessary to update")
+                    .setMessage("You should update your application here: " + BuildConfig.UPDATE_URL);
+        } else {
+            alertDialogBuilder
+                    .setTitle("You want to update?")
+                    .setMessage("Do you want to update application? ")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(final DialogInterface dialog, final int id) {
+                            MainActivity.this.finish();
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(final DialogInterface dialog, final int id) {
+                            dialog.cancel();
+                        }
+                    });
+        }
+        alertDialogBuilder
+                .setCancelable(false)
+                .create()
+                .show();
     }
 }
